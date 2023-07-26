d88f0783b7e88cc9699eaa03208541af2468e620
Pepesaur96 <jjvazquez96@gmail.com>
2023-07-26 17:36:18 +0000

    full header proto

--- main.h
+++ main.h
@@ -8,4 +8,22 @@
 int _putchar(char);
 int _printf(const char *format, ...);
 
+/**
+ * struct format_types - struct used to define function
+ * @id: stuff
+ * @f: pointer to function
+ */
+
+typedef struct format_types
+{
+char *id;
+void (*f)(const char *format, va_list args);
+} f_t;
+
+void print_char(const char *format, va_list args);
+void print_int_i(const char *format, va_list args);
+void print_int_d(const char *format, va_list args);
+void print_f(const char *format, va_list args);
+void print_str(const char *format , va_list args);
+
 #endif
