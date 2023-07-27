2a3692cc787c2e89a1cbc04046fdaeec66279580
Pepesaur96 <jjvazquez96@gmail.com>
2023-07-27 18:43:25 +0000

    BBranchupdated printf.c

--- _printf.c
+++ _printf.c
@@ -21,13 +21,14 @@ int _printf(const char *format, ...)
 	unsigned int i, j;
 	int count = 0;
 	va_list args;
+	char c;
 
 	f_t f_types[] = {
-		{'c', &print_char},
-		{'i', &print_int_i},
-		{'d', &print_int_d},
-		{'%', &print_percent},
-		{'s', &print_str},
+		{"c", print_char},
+		{"i", print_int_i},
+		{"d", print_int_d},
+		{"%", print_percent},
+		{"s", print_str},
 		{NULL, NULL}
 	};
 	va_start(args, format);
@@ -49,16 +50,17 @@ int _printf(const char *format, ...)
 					if (format[i] == *f_types[j].id)
 						count += f_types[j].f(args);
 					else
+					{
 						i--;
-						char c = format[i];
+						c = format[i];
 						write(1, &c, 1);
 						count++;
+					}
 				}
 			}
 		}
 	}
 	format++;
-	}
 	va_end(args);
 	printf("\n");
 	return (count);
